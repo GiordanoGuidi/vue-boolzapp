cdf487bd51bd40f0a2ce3564af51d8fa0dabef53
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-01-24 11:18:40 +0100

    added scaffolding

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+console.log('HELLO WORLD')
+
+const {createApp} = Vue
+const app = createApp({
+    data(){
+        return{
+            user:user,
+            contacts:contacs
+        }
+    }
+})
+console.log('Vue ok', Vue)
