891c99abc9c6732b19bf4d828b7a331ca0009628
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-01-27 20:13:26 +0100

    added file responsive

--- JS/script.js
+++ JS/script.js
@@ -10,91 +10,107 @@ const app = createApp({
           activeId: 1,
           newMessage:'',
           reserchText:'',
-          validMessages:[],
+          hasSearchMessage:false,
+          reserchMessageText:''
         }
     },
     computed:{
 
     //#COMPUTED---------------------------------
-      //CONTATTO ATTIVO
-      currentContact(){
-        return this.contacts.find(contact=> contact.id === this.activeId)
-      },
-      // MESSAGGI DEL CONTATTO ATTIVO
-      currentContactMessages(){
-        return this.currentContact.messages;
+    // MESSAGGI DEL CONTATTO ATTIVO
+    currentContactMessages(){
+      return this.currentContact.messages;
+    },
+    //CONTATTO ATTIVO
+    currentContact(){
+      return this.contacts.find(contact=> contact.id === this.activeId)
+    },
+    
+    //ARRAY FILTRATO IN BASE AL RESERCHTEXT
+    filteredContacts(){
+      const searchTerm = this.reserchText.toLowerCase();
+      const filteredArray = this.contacts.filter(contact=>
+        contact.name.toLowerCase().includes(searchTerm)
+        );
+        return filteredArray;
       },
       
-      //ARRAY FILTRATO IN BASE AL RESERCHTEXT
-      filteredContacts(){
-        const searchTerm = this.reserchText.toLowerCase();
-        const filteredArray = this.contacts.filter(contact=>
-          contact.name.toLowerCase().includes(searchTerm)
-          );
-          return filteredArray;
-        },
-        
-        
-        
+    
+    },
+    methods:{
+      
+      //#METHODS----------------------
+      //RECUPERO ID DEL CONTATTO
+      getActiveId(contact){
+        this.activeId = contact.id;
       },
-      methods:{
+      
+      
+      // CREO FUNZIONE CHE GENERA IL MESSAGGIO 
+      addNewMessage(text,status){
+        const message = {
+          id:new Date().getTime(),
+          date: new Date().toLocaleTimeString(),
+          text,
+          status
+        }        
+        this.currentContactMessages.push(message)
+        this.newMessage ='';
+      },
+      
+      /* CREO FUNZIONE CHE INVOCA LA FUNZIONE CHE GENERA IL MESSAGGIO
+      E AGGIUNGO SET TIMEOUT PER LA RISPOSTA*/
+      sendMessage(){
+        if(!this.newMessage)return;
+        this.addNewMessage(this.newMessage,'received');
+        setTimeout(()=>{
+          this.addNewMessage('ok','sent');
+        },1000)},
         
-        //#METHODS----------------------
-        //RECUPERO ID DEL CONTATTO
-        getActiveId(contact){
-          this.activeId = contact.id;
-        },
-        
-        
-        // CREO FUNZIONE CHE GENERA IL MESSAGGIO 
-        addNewMessage(text,status){
-          const message = {
-            id:new Date().getTime(),
-            date: new Date().toLocaleTimeString(),
-            text,
-            status
-          }
-          this.currentContactMessages.push(message)
-          this.newMessage ='';
-        },
-        
-        /* CREO FUNZIONE CHE INVOCA LA FUNZIONE CHE GENERA IL MESSAGGIO
-        E AGGIUNGO SET TIMEOUT PER LA RISPOSTA*/
-        sendMessage(){
-          if(!this.newMessage)return;
-          this.addNewMessage(this.newMessage,'received')
-          setTimeout(()=>{
-            this.addNewMessage('ok','sent');
-          },1000)},
-          
-          
-          
+      //! FILTRO ARRAY DAI MESSAGGI SENZA IL MESSAGGIO DA ELIMINARE
+      //!(PROBLEMA E' CHE LE COMPUTED NON POSSONO ESSERE MODIFICATE)
         // deleteMessage(id){
+        //   this.currentContactMessages = this.currentContactMessages.filter(message =>{
+        //     if(id !== message.id){
+        //       console.log('ciao')
+        //       return true
+        //     }else{
+        //       return false
+        //     }
+        //   })
+        //   return this.currentContactMessages 
           
-          //   this.currentContactMessages = this.currentContactMessages.filter(message =>{
-            //     if(id !== message.id){
-              //       return true
-              //     }else{
-                //       return false
-                //     }
-                //   })
-                
-                //   console.log('id', id,'lista messaggi-----',
-                //   this.currentContactMessages,'messaggi validi----',
-                //   this.currentContactMessages,'eo', this.currentContact)
-                // },
-                  
-          // RECUPERO IL TESTO DELL'ULTIMO MESSAGGIO DEL CONTATTO
-          getLastMessage(contact){
+        //   console.log('id', id,'lista messaggi-----',
+        //   this.currentContactMessages,'messaggi validi----',
+        //   validMessages,)
+        // },
+
+
+        // RECUPERO IL TESTO DELL'ULTIMO MESSAGGIO DEL CONTATTO
+        getLastMessage(contact){
           const lastMessage = contact.messages[contact.messages.length - 1]
           return lastMessage
-          }
-                  
-          
-        } ,
-        
-        created(){
-          
+        },
+
+        //CREO FUNZIONE CHE CAMBIA IL VALORE DI HAS SEARCH MESSAGE
+        changeHasSearchMessage(){
+          this.hasSearchMessage = true;
         }
-      });
-      app.mount('#root');
+
+        //!CREO ARRAY CHE FILTRA IL TESTO DEL MESSAGGIO CERCATO
+        //!(STESSA DIFFICOLTA' DELLA RIMOZIONE MESSAGGIO)
+
+        
+
+       
+        
+
+        
+    },       
+      
+      
+    created(){
+      
+    }
+    });
+    app.mount('#root');
